949049a4ff80b356ce030c059c4b25428473ab12
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-05-14 21:05:08 +0530

    new update 76

--- public/Flow Control/forloop.js
+++ public/Flow Control/forloop.js
@@ -7,48 +7,48 @@ for (var i_2 = 0; i_2 < 5; i_2++) {
     console.log(i_2);
 }
 console.log(i); //i is not defined
-for (var i = 0; i < 5; i++) {
-    console.log(i);
+for (var i1 = 0; i1 < 5; i1++) {
+    console.log(i1);
 }
-console.log(i); //5
-var i = 0;
-for (;; i++) {
-    console.log(i);
+console.log(i1); //5
+var i2 = 0;
+for (;; i2++) {
+    console.log(i2);
 }
-var i = 0;
-for (;; i++) {
-    if (i >= 5)
+var i3 = 0;
+for (;; i3++) {
+    if (i3 >= 5)
         break;
-    console.log(i);
+    console.log(i3);
 }
-var i = 0;
+var i4 = 0;
 for (;;) {
-    if (i >= 5)
+    if (i4 >= 5)
         break;
-    console.log(i);
+    console.log(i4);
     i++;
 }
-for (var i_3 = 0; i_3 < 5; i_3++) {
-    console.log(i_3);
+for (var i5 = 0; i5 < 5; i5++) {
+    console.log(i5);
 }
-var i = 0;
-for (var i_4 = 0; i_4 < 5; i_4++) {
-    if (i_4 < 2)
+var i6 = 0;
+for (var i6_1 = 0; i6_1 < 5; i6_1++) {
+    if (i6_1 < 2)
         continue;
-    console.log(i_4);
+    console.log(i6_1);
 }
-var i = 0;
-for (var i_5 = 5; i_5 > 0; i_5--) {
-    console.log(i_5);
+var i7 = 0;
+for (var i7_1 = 5; i7_1 > 0; i7_1--) {
+    console.log(i);
 }
-for (var i_6 = 0, j_1 = 0; i_6 < 5; i_6 = j_1 + 1, j_1++, i_6++) {
-    console.log(i_6 + j_1);
+for (var i8 = 0, j8 = 0; i8 < 5; i8 = j8 + 1, j8++, i8++) {
+    console.log(i8 + j8);
 }
 var sum = 0;
-for (var i_7 = 0; i_7 <= 5; sum = sum + i_7, i_7++)
+for (var i_3 = 0; i_3 <= 5; sum = sum + i_3, i_3++)
     ;
 console.log(sum);
 var persons = new Array('John', 'Ann', 'Aaron', 'Edwin', 'Elizabeth');
-for (var i_8 = 0; i_8 < persons.length; i_8++) {
-    console.log(persons[i_8]);
+for (var i_4 = 0; i_4 < persons.length; i_4++) {
+    console.log(persons[i_4]);
 }
--- src/Flow Control/forloop.ts
+++ src/Flow Control/forloop.ts
@@ -9,47 +9,47 @@ for (let i = 0; i < 5; i++) {
 
 console.log(i); //i is not defined
 
-for (var i = 0; i < 5; i++) {
-  console.log(i);
+for (var i1 = 0; i1 < 5; i1++) {
+  console.log(i1);
 }
 
-console.log(i); //5
+console.log(i1); //5
 
-let i: number = 0;
-for (; ; i++) {
-  console.log(i);
+let i2: number = 0;
+for (; ; i2++) {
+  console.log(i2);
 }
 
-let i = 0;
-for (; ; i++) {
-  if (i >= 5) break;
-  console.log(i);
+let i3 = 0;
+for (; ; i3++) {
+  if (i3 >= 5) break;
+  console.log(i3);
 }
 
-let i = 0;
+let i4 = 0;
 for (;;) {
-  if (i >= 5) break;
-  console.log(i);
+  if (i4 >= 5) break;
+  console.log(i4);
   i++;
 }
 
-for (let i = 0; i < 5; i++) {
+for (let i5 = 0; i5 < 5; i5++) {
+  console.log(i5);
+}
+
+let i6 = 0;
+for (let i6 = 0; i6 < 5; i6++) {
+  if (i6 < 2) continue;
+  console.log(i6);
+}
+
+let i7 = 0;
+for (let i7 = 5; i7 > 0; i7--) {
   console.log(i);
 }
 
-let i = 0;
-for (let i = 0; i < 5; i++) {
-  if (i < 2) continue;
-  console.log(i);
-}
-
-let i = 0;
-for (let i = 5; i > 0; i--) {
-  console.log(i);
-}
-
-for (let i = 0, j = 0; i < 5; i = j + 1, j++, i++) {
-  console.log(i + j);
+for (let i8 = 0, j8 = 0; i8 < 5; i8 = j8 + 1, j8++, i8++) {
+  console.log(i8 + j8);
 }
 
 let sum = 0;
